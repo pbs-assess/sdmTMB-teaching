3dc8a199b1dc81bd4d47b35a77040dba9763709b
Sean Anderson <sean@seananderson.ca>
2022-02-23 18:28:19 -0800

    Break slides into 2; more to come

---
+++ noaa-psaw-2022/preamble.R
@@ -0,0 +1,11 @@
+xaringanthemer::style_mono_accent(
+  base_color = "#202020",
+  header_font_google = xaringanthemer::google_font("Raleway"),
+  text_font_google = xaringanthemer::google_font("Open Sans"),
+  code_font_google = xaringanthemer::google_font("Fira Mono"),
+  base_font_size = "20px",
+  header_h1_font_size = "2.4rem",
+  text_font_size = "1.5rem",
+  code_font_size = "1.3rem",
+  link_color = "#0047AB"
+)
