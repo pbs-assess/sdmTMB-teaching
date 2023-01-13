d74fd29b5bd9960bd67ed742055ce8deef80e7a4
Sean Anderson <sean@seananderson.ca>
2023-01-13 10:04:28 -0800

    More ex

--- dfo-tesa-2023/exercises/99-make-answers.R
+++ dfo-tesa-2023/exercises/99-make-answers.R
@@ -5,7 +5,7 @@ f <- f_ans[!grepl("# answer$", f_ans)]
 for (i in seq_along(f)) {
   if (grepl(" # exercise$", f[i])) {
     f[i] <- gsub("^# ", "  ", f[i])
-    f[i] <- gsub("^#([a-zA-Z]+)", "\\1", f[i])
+    f[i] <- gsub("^#([a-zA-Z\\#]+)", "\\1", f[i])
   }
 }
 
